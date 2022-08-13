84bf152cb98932a7ab2720cbbc1e7519b265fc1d
vinylcrackle <trqsib@hotmail.com>
2022-08-13 19:24:32 +0200

    address review requests

--- source/pgnloader.py
+++ source/pgnloader.py
@@ -4,11 +4,14 @@ import pathlib
 
 class PgnFileLoader:
 
+    pgn_file = None
+    pgn_data_instance = None
+
     def __init__(self, pgn_file_path):
         if pathlib.Path.exists(pgn_file_path):
             self.pgn_file = open(pgn_file_path)
 
-        self.pgnDataInstance = pgnlib.read_game(self.pgn_file)
+        self.pgn_data_instance = pgnlib.read_game(self.pgn_file)
 
     def get_pgn_data_instance(self):
-        return self.pgnDataInstance
+        return self.pgn_data_instance
